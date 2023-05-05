5c4de8c7224187ddaf7ae6b944135a6de6a69dce
FeelingXD <wlals425315@gmail.com>
2023-05-05 14:36:27 +0900

    Add : Project Preset

---
+++ src/main/java/com/zerobase/moy/MoyApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.moy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MoyApplication.class, args);
+  }
+
+}
