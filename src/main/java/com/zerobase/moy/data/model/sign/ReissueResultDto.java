a9471a1edaa10d69fd5f4907865e371e42f82ab0
FeelingXD <wlals425315@gmail.com>
2023-05-15 18:27:51 +0900

    Rename: Change dir
    패키지 변경

--- src/main/java/com/zerobase/moy/data/model/sign/ReissueResultDto.java
+++ src/main/java/com/zerobase/moy/data/model/sign/ReissueResultDto.java
@@ -1,21 +1,18 @@
-package com.zerobase.moy.data.model;
+package com.zerobase.moy.data.model.sign;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Getter
-@Setter
-public class SignUpResultDto {
+@Builder
+public class ReissueResultDto {
 
-  private boolean success;
+  TokenDto token;
 
-  private int code;
-
-  private String msg;
 }
