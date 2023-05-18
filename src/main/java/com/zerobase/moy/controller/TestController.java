426bcadca5749c676ca2fd5f2e6b6c173891ae4e
FeelingXD <wlals425315@gmail.com>
2023-05-19 06:08:01 +0900

    Update: test Api

--- src/main/java/com/zerobase/moy/controller/TestController.java
+++ src/main/java/com/zerobase/moy/controller/TestController.java
@@ -1,7 +1,12 @@
 package com.zerobase.moy.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.zerobase.moy.data.entity.User;
+import com.zerobase.moy.response.ApiResponse;
+import com.zerobase.moy.response.ResponseCode;
+import com.zerobase.moy.service.impl.TestServiceImpl;
 import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/test")
+@RequiredArgsConstructor
 public class TestController {
-
+  private final TestServiceImpl testService;
   @GetMapping()
   public ResponseEntity<?> test(HttpServletRequest req) {
     System.out.println(req);
@@ -23,5 +29,20 @@ public class TestController {
     return ResponseEntity.ok().body(user.getId());
   }
 
-
+  @GetMapping("/report")
+  public ResponseEntity<?> testReport() throws JsonProcessingException {
+    var result=testService.test();
+    return ResponseEntity.ok().body(ApiResponse.builder()
+        .code(ResponseCode.RESPONSE_SUCCESS)
+        .data(result)
+        .build());
+  }
+  @GetMapping("/reportException")
+  public ResponseEntity<?> testException() throws JsonProcessingException {
+    var result=testService.testException();
+    return ResponseEntity.ok().body(ApiResponse.builder()
+        .code(ResponseCode.RESPONSE_SUCCESS)
+        .data(result)
+        .build());
+  }
 }
