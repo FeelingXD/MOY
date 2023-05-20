be7ebed0b1c88108fec5478c545c204d3d443b9f
FeelingXD <wlals425315@gmail.com>
2023-05-21 03:51:20 +0900

    Update: JsonProcessingException handler
    Json 응답값 오류에대한 advice 설정

--- src/main/java/com/zerobase/moy/response/exception/CustomExceptionAdvise.java
+++ src/main/java/com/zerobase/moy/response/exception/CustomExceptionAdvise.java
@@ -1,9 +1,14 @@
 package com.zerobase.moy.response.exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.zerobase.moy.data.model.CLOVA.SentimentErrorResponse;
 import com.zerobase.moy.response.ApiResponse;
+import com.zerobase.moy.util.JsonUtil;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import springfox.documentation.spring.web.json.Json;
 
 @RestControllerAdvice
 public class CustomExceptionAdvise {
@@ -14,6 +19,19 @@ public class CustomExceptionAdvise {
         .body(ApiResponse.builder()
             .code(e.getErrorCode())
             .build()
+        );
+  }
+  @ExceptionHandler(JsonProcessingException.class)
+  public  ResponseEntity<?> JsonExceptionResponseEntity(JsonProcessingException e)
+      throws Exception {
+    String rawContent = e.getLocation().contentReference().getRawContent().toString();
+    SentimentErrorResponse errorResponse = JsonUtil.toSentimentErrorResponse(rawContent);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(
+            ApiResponse.builder()
+                .code(ErrorCode.CLOVA_RESPONSE_ERROR)
+                .data(errorResponse)
+                .build()
         );
   }
 }
